da3616fdbcd37a5427fcd123d53b5b02ee77c49e
Omar <omarftt@gmail.com>
2022-02-05 12:36:19 -0500

    '5th'

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.get('/makers/:nombre',(req,res) => {
-    /*
-    const nameS = req.params.nombre;
-    const nameS2 = nameS.charAt(0).toUpperCase() + nameS.slice(1);
-    res.send("<h1>Hola " +nameS2 + "!</h1>");*/
-    const nameS2 = req.params.nombre;
-    var nameS = ''
-    console.log(nameS2 , nameS2.length)
+app.get('/',(req,res) => {
 
-    if (nameS2.length > 0) {
-        nameS = nameS2.charAt(0).toUpperCase() + nameS2.slice(1);
+    for(var i = 1; i<= 50; i++){
+        if (i%2 == 0) {
+            res.write(i + " Soy Par!\n");
+        }
+        else {
+            res.write(i + " Soy Impar!\n");
+        }
     }
-    else {
-        nameS = 'desconocido'
-    }
-    
-    res.send("<h1>Hola " +nameS + "!</h1>");
-    
+    res.end();
     
 });
 
